d88edb24fbbce102e93f3dcc50fe0fe62cb18f66
samniem <sami.nieminen.hw@gmail.com>
2019-04-07 19:05:54 +0300

    Heroku test 8 of deployment

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if(process.env.NODE_ENV === 'production') {
 }
 
 //dev mode
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'/client/public/index.html'))
 })
 
